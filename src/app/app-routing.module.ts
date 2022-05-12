c78da16142619f15257e0bf71260c9c768378fd0
Amir <amir.a.rezaei.p@gmail.com>
2022-05-12 20:31:22 +0430

    Fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,16 +4,16 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'products' },
   {
-    path: 'products',
-    loadChildren: () => import('./products/products.module').then(m => m.ProductsModule),
-  },
-  {
     path: 'orders',
     loadChildren: () => import('./orders/orders.module').then(m => m.OrdersModule)
   },
   {
     path: 'persons',
     loadChildren: () => import('./persons/persons.module').then(m => m.PersonsModule)
+  },
+  {
+    path: 'products',
+    loadChildren: () => import('./products/products.module').then(m => m.ProductsModule),
   },
 ];
 
